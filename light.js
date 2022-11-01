5acab6371cfbc2bec9f5ae5b47a780fe38f50180
DEVVRAT SARKAR <104255495+devvratsarkar@users.noreply.github.com>
2022-10-31 22:18:21 -0500

    Add files via upload

---
+++ light.js
@@ -0,0 +1,9 @@
+ON=()=>{
+    const Bulb_on=document.getElementById("BulbImg")
+    Bulb_on.src='bulb on.png';
+}
+
+Off=()=>{
+    const Bulb_on=document.getElementById("BulbImg")
+    Bulb_on.src='bulb off.png'
+}
